93862824bf00bae17665d3305113e84a7679dc2e
Tade Maddonni <tadeomaddonni@gmail.com>
2022-10-13 15:48:29 -0300

    desafio 2

--- desafio.js
+++ desafio.js
@@ -1,4 +1,6 @@
-class Usuarios {
+const fs = require("fs");
+
+/* class Usuarios {
   constructor(
     nombre = "",
     apellido = "",
@@ -50,3 +52,98 @@ addMascota("Yin yan");
 console.log("libros: " + getBookNames());
 console.log("Nombre completo: " +getFullName());
 console.log("Mascotas: " + getMascotas());
+ */
+/* 
+fs.readFile("./fyh.txt", "utf8", (error, contenido) => {
+  if (error) {
+    console.log("Ha surgido un error");
+  } else {
+    console.log("Operacion exitosa: " + contenido);
+  }
+}); */
+
+class Contenedor {
+	constructor(ruta) {
+		this.ruta = ruta;
+	}
+
+	productos = [];
+	async escribirProductos() {
+		await fs.promises.writeFile(
+			this.ruta,
+			JSON.stringify(this.productos, null, 2)
+		);
+	}
+	async save(object) {
+		const archivo = await fs.promises.readFile(this.ruta, "utf8");
+		if (archivo.length < 1 || archivo === "[]") {
+			this.productos.push({ ...object, id: this.productos.length + 1 });
+			this.escribirProductos();
+		} else {
+			try {
+				const data = await fs.promises.readFile(this.ruta, "utf-8");
+				const res = await JSON.parse(data);
+				if (res.length >= 1) {
+					this.productos = res;
+					this.productos.push({
+						...object,
+						id: this.productos[this.productos.length - 1].id + 1,
+					});
+					this.escribirProductos();
+					console.log(this.productos);
+				} else {
+					this.productos = [
+						res,
+						{
+							...object,
+							id: this.productos.length + 1,
+						},
+					];
+					this.escribirProductos();
+				}
+			} catch (error) {
+				console.log("Error: " + error);
+			}
+		}
+	}
+	async getById(id) {
+		try {
+			const data = await fs.promises.readFile(this.ruta, "utf-8");
+			const res = await JSON.parse(data);
+			const item = await res.find((el) => el.id === id);
+			console.log(item);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	async getAll() {
+		try {
+			const data = await fs.promises.readFile(this.ruta, "utf-8");
+			const res = await JSON.parse(data);
+			this.productos = await res;
+			console.log(this.productos);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	async deleteById(id) {
+		try {
+			const data = await fs.promises.readFile(this.ruta, "utf-8");
+			const res = await JSON.parse(data);
+			this.productos = await res.filter((el) => el.id != id);
+			await fs.promises.writeFile(
+				this.ruta,
+				JSON.stringify(this.productos, null, 2)
+			);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	async deleteAll() {
+		this.productos = [];
+		this.escribirProductos();
+	}
+}
+
+const contenedor = new Contenedor("./productos.json");
+contenedor.save({name:"tade", apellido:"maddonni"})
