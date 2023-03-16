46428b4d17355cfa547013d6114c9c989fd4076b
Tade Maddonni <tadeomaddonni@gmail.com>
2023-03-16 19:24:47 -0300

    adding mocha tests

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,14 +1,21 @@
-import { UserMongoManager } from "../DB/managers/mongo.manager.js";
-import { userModel } from "../DB/models/UserModels.js";
-import { connectMongo } from "../config/database/dbConnection.js";
+import { productContainer } from "../server.js";
 import assert from "assert";
 
 describe("Test de la clase de usuarios", async () => {
-	it("Test de la función de obtener un usuario", async () => {
-		connectMongo();
-		const instance = new UserMongoManager(userModel);
-		const user = await instance.getUser("tade@gg.com");
-		console.log(user);
-		assert.equal(user, true);
+	it("Test de la función de obtener los productos", async () => {
+		const products = await productContainer.getProducts();
+
+		assert.equal(products.length, 5);
+	});
+
+	it("Test de la función de agregar un producto", async () => {
+		const product = {
+			name: "Producto test",
+			price: 100,
+			thumbnail: "https://picsum.photos/200/300",
+		};
+		const products = await productContainer.addProduct(product);
+
+		assert.equal(products, "Producto agregado correctamente");
 	});
 });
